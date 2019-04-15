ba88848da13545dfa82960c9734afd7d5b3cf6c8
Zaur Kurbanismailov <kurbanismailov.92@gmail.com>
2019-04-16 01:40:27 +0300

    Now ModelManager can import, clamp and center model

--- Assets/Scripts/Managing/ModelManager.cs
+++ Assets/Scripts/Managing/ModelManager.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using static UnityEngine.Debug;
 using Visuals;
 using Dummiesman;
+using Utilities.Geometry;
 
 namespace Managing
 {
@@ -12,16 +13,42 @@ namespace Managing
 	{
         private Model _model;
 
+        [SerializeField]
+        [Range(0f, 32f)]
+        private float _allowedMaxSize;
+
         public void Import(string path)
         {
             var go = new OBJLoader().Load(path);
 
-            Clamp(go, 30f);
+            var bounds = GetBounds(go);
+
+            Clamp(go, ref bounds);
+            Center(go, bounds);
+
         }
 
-        private void Clamp(GameObject go, float size)
+        private Bounds GetBounds(GameObject go)
         {
-
+            return BoundsUtility.GetGlobalBounds(go, BoundsUtility.BoundsCreateOption.Mesh);
         }
-	}
+
+        private void Clamp(GameObject go, ref Bounds bounds)
+        {
+            var maxSize = Mathf.Max(bounds.size.x, bounds.size.y, bounds.size.z);
+
+            if (maxSize <= _allowedMaxSize) return;
+
+            float ratio = _allowedMaxSize / maxSize;
+
+            go.transform.localScale *= ratio;
+            bounds.center *= ratio;
+            bounds.size *= ratio; 
+        }
+
+        private void Center(GameObject go, Bounds bounds)
+        {
+            go.transform.position -= bounds.center;
+        }
+    }
 }
---
+++ Assets/Scripts/Utilitiies/Geometry/BoundsUtility.cs
@@ -0,0 +1,117 @@
+﻿using Utilities.Geometry.Exceptions;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Utilities.Geometry
+{
+    public static class BoundsUtility
+    {
+        public enum BoundsCreateOption
+        {
+            Transform,
+            Mesh,
+            TransformAndMesh
+        }
+        
+        public static RotatableBounds GetLocalBound(GameObject gameObject, BoundsCreateOption option)
+        {
+            RotatableBounds rotatableBounds = new RotatableBounds();
+
+            bool isCenterSettedByTransform = false;
+
+            if (option == BoundsCreateOption.Transform || option == BoundsCreateOption.TransformAndMesh)
+            {
+                Transform[] transforms = gameObject.GetComponentsInChildren<Transform>();
+
+                rotatableBounds = new RotatableBounds(transforms[0].position, transforms[0].rotation);
+                isCenterSettedByTransform = true;
+
+                for (int i = 1; i < transforms.Length; i++)
+                {
+                    rotatableBounds.Encapsulate(transforms[i].position);
+                }
+            }
+
+            bool isCenterSettedByMesh = false;
+
+            if (option == BoundsCreateOption.Mesh || option == BoundsCreateOption.TransformAndMesh)
+            {
+                MeshFilter[] meshFilters = gameObject.GetComponentsInChildren<MeshFilter>();
+
+                if (!isCenterSettedByTransform)
+                {
+                    if (meshFilters.Length == 0)
+                    {
+                        throw new BoundsCreationException("Object and it's childs not have MeshFilter component");
+                    }
+                    else
+                    {
+                        Vector3 boundsCenter = meshFilters[0].transform.TransformPoint(meshFilters[0].sharedMesh.bounds.center);
+                        rotatableBounds = new RotatableBounds(boundsCenter, gameObject.transform.rotation);
+                    }
+
+                    isCenterSettedByMesh = true;
+                }
+
+                for (int i = 0; i < meshFilters.Length; i++)
+                {
+                    Bounds meshBounds = meshFilters[i].sharedMesh.bounds;
+                    meshBounds.center = meshFilters[i].transform.TransformPoint(meshBounds.center);
+                    meshBounds.size = Vector3.Scale(meshBounds.size, meshFilters[i].transform.lossyScale);
+
+                    rotatableBounds.Encapsulate(new RotatableBounds(meshBounds, meshFilters[i].transform.rotation));
+                }
+            }
+
+            return rotatableBounds;
+        }
+
+        public static Bounds GetGlobalBounds(GameObject gameObject, BoundsCreateOption option)
+        {
+            Bounds bounds = new Bounds();
+
+            bool isCenterSettedByTransform = false;
+
+            if (option == BoundsCreateOption.Transform || option == BoundsCreateOption.TransformAndMesh)
+            {
+                Transform[] transforms = gameObject.GetComponentsInChildren<Transform>();
+
+                bounds.center = transforms[0].position;
+                isCenterSettedByTransform = true;
+
+                for (int i = 1; i < transforms.Length; i++)
+                {
+                    bounds.Encapsulate(transforms[i].position);
+                }
+            }
+
+            bool isCenterSettedByMesh = false;
+
+            if (option == BoundsCreateOption.Mesh || option == BoundsCreateOption.TransformAndMesh)
+            {
+                MeshRenderer[] meshRenderers = gameObject.GetComponentsInChildren<MeshRenderer>();
+
+                if (!isCenterSettedByTransform)
+                {
+                    if (meshRenderers.Length == 0)
+                    {
+                        throw new BoundsCreationException("Object and it's childs not have MeshFilter component");
+                    }
+                    else
+                    {
+                        bounds = meshRenderers[0].bounds;
+                    }
+                }
+
+                for (int i = isCenterSettedByTransform ? 0 : 1; i < meshRenderers.Length; i++)
+                {
+                    bounds.Encapsulate(meshRenderers[i].bounds);
+                }
+            }
+
+            return bounds;
+        }
+    }
+}
