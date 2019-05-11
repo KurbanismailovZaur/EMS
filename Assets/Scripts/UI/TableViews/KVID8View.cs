d8d4402fcca60515b1e7b55935ae3978999c4cea
nikita <koz.nikita@gmail.com>
2019-05-11 17:29:42 +0300

    kvid8 changes

--- Assets/Scripts/UI/TableViews/KVID8View.cs
+++ Assets/Scripts/UI/TableViews/KVID8View.cs
@@ -9,6 +9,7 @@ using UI.TableViews.IO;
 using UI.Tables.Concrete.KVIDS;
 using Management.Calculations;
 using Management.Tables;
+using UI.Tables;
 
 namespace UI.TableViews
 {
@@ -16,6 +17,42 @@ namespace UI.TableViews
     {
         [SerializeField]
         private Button _importButton;
+
+        [Header("Observers")]
+        [SerializeField]
+        private ColumnObserver _removesObserverTab0;
+
+        [SerializeField]
+        private ColumnObserver _codesObserverTab0;
+
+        [SerializeField]
+        private ColumnObserver _maxValueObserverTab0;
+
+        [SerializeField]
+        private ColumnObserver _minFObserverTab0;
+
+        [SerializeField]
+        private ColumnObserver _maxFObserverTab0;
+
+
+        [SerializeField]
+        private ColumnObserver _removesObserverTab1;
+
+        [SerializeField]
+        private ColumnObserver _codesESObserverTab1;
+
+        [SerializeField]
+        private ColumnObserver _codesWireObserverTab1;
+
+        [SerializeField]
+        private ColumnObserver _maxValueObserverTab1;
+
+        [SerializeField]
+        private ColumnObserver _minFObserverTab1;
+
+        [SerializeField]
+        private ColumnObserver _maxFObserverTab1;
+
 
         [Header("Other")]
         [SerializeField]
@@ -31,6 +68,8 @@ namespace UI.TableViews
             _importButton.onClick.AddListener(Import_OnClick);
 
             SelectFirstTab();
+
+            SubsribeObservers();
         }
 
         public override void Open()
@@ -126,6 +165,48 @@ namespace UI.TableViews
                 panel.MaxVoltage.FloatValue = maxVoltage;
                 panel.FrequencyMin.IntValue = fMin;
                 panel.FrequencyMax.IntValue = fMax;
+            }
+
+        }
+
+
+        private void SubsribeObservers()
+        {
+            var tab0Table = (KVID8Tab0Table)_tabsAssociations[0].table;
+            var tab1Table = (KVID8Tab1Table)_tabsAssociations[1].table;
+
+            if (tab0Table)
+            {
+                tab0Table.Removes.RectTransformChanged.AddListener(_removesObserverTab0.Column_RectTransformChanged);
+                tab0Table.IDs.RectTransformChanged.AddListener(_codesObserverTab0.Column_RectTransformChanged);
+                tab0Table.MaxVoltages.RectTransformChanged.AddListener(_maxValueObserverTab0.Column_RectTransformChanged);
+                tab0Table.FrequencyMins.RectTransformChanged.AddListener(_minFObserverTab0.Column_RectTransformChanged);
+                tab0Table.FrequencyMaxs.RectTransformChanged.AddListener(_maxFObserverTab0.Column_RectTransformChanged);
+
+
+                _removesObserverTab0.Column_RectTransformChanged(((RectTransform)tab0Table.Removes.transform).sizeDelta);
+                _codesObserverTab0.Column_RectTransformChanged(((RectTransform)tab0Table.IDs.transform).sizeDelta);
+                _maxValueObserverTab0.Column_RectTransformChanged(((RectTransform)tab0Table.MaxVoltages.transform).sizeDelta);
+                _minFObserverTab0.Column_RectTransformChanged(((RectTransform)tab0Table.FrequencyMins.transform).sizeDelta);
+                _maxFObserverTab0.Column_RectTransformChanged(((RectTransform)tab0Table.FrequencyMaxs.transform).sizeDelta);
+            }
+
+            if (tab1Table)
+            {
+                tab1Table.Removes.RectTransformChanged.AddListener(_removesObserverTab1.Column_RectTransformChanged);
+                tab1Table.IDs.RectTransformChanged.AddListener(_codesESObserverTab1.Column_RectTransformChanged);
+                tab1Table.WireIDs.RectTransformChanged.AddListener(_codesWireObserverTab1.Column_RectTransformChanged);
+                tab1Table.MaxVoltages.RectTransformChanged.AddListener(_maxValueObserverTab1.Column_RectTransformChanged);
+                tab1Table.FrequencyMins.RectTransformChanged.AddListener(_minFObserverTab1.Column_RectTransformChanged);
+                tab1Table.FrequencyMaxs.RectTransformChanged.AddListener(_maxFObserverTab1.Column_RectTransformChanged);
+
+
+                _removesObserverTab1.Column_RectTransformChanged(((RectTransform)tab1Table.Removes.transform).sizeDelta);
+                _codesESObserverTab1.Column_RectTransformChanged(((RectTransform)tab1Table.IDs.transform).sizeDelta);
+                _codesWireObserverTab1.Column_RectTransformChanged(((RectTransform)tab1Table.WireIDs.transform).sizeDelta);
+                _maxValueObserverTab1.Column_RectTransformChanged(((RectTransform)tab1Table.MaxVoltages.transform).sizeDelta);
+                _minFObserverTab1.Column_RectTransformChanged(((RectTransform)tab1Table.FrequencyMins.transform).sizeDelta);
+                _maxFObserverTab1.Column_RectTransformChanged(((RectTransform)tab1Table.FrequencyMaxs.transform).sizeDelta);
             }
 
         }
