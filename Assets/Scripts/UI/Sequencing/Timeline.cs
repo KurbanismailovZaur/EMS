e14fa976b16c1cfc0a69674b4a5943272b14666f
Zaur Kurbanismailov <kurbanismailov.92@gmail.com>
2019-05-14 01:53:29 +0300

    Timeline completed

---
+++ Assets/Scripts/UI/Sequencing/Timeline.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading.Tasks;
+using static UnityEngine.Debug;
+using UnityEngine.UI;
+using System.Globalization;
+using System;
+using UnityEngine.Events;
+
+namespace UI.Sequencing
+{
+	public class Timeline : MonoSingleton<Timeline> 
+	{
+        [Serializable]
+        public class ChangedEvent : UnityEvent<int> { }
+
+        [SerializeField]
+        private CanvasGroup _canvasGroup;
+
+        [SerializeField]
+        private Slider _slider;
+
+        [SerializeField]
+        private Text _timeText;
+
+        [SerializeField]
+        private Button _playButton;
+
+        [SerializeField]
+        private GameObject _playImage;
+
+        [SerializeField]
+        private GameObject _pauseImage;
+
+        private const float _timeDelta = 1f / 36f;
+
+        private static NumberFormatInfo _nfi = new NumberFormatInfo { NumberDecimalSeparator = "." };
+
+        private Coroutine _playRoutine;
+
+        public ChangedEvent Changed;
+
+        private void Start()
+        {
+            _slider.onValueChanged.AddListener(Slider_OnValueChanged);
+            _playButton.onClick.AddListener(PlayButton_OnClick);
+        }
+
+        public void Show()
+        {
+            SetCanvasGroupParameters(1f, true);
+        }
+
+        public void Hide()
+        {
+            SetCanvasGroupParameters(0f, false);
+        }
+
+        private void SetCanvasGroupParameters(float alpha, bool blockRaycast)
+        {
+            _canvasGroup.alpha = alpha;
+            _canvasGroup.blocksRaycasts = blockRaycast;
+        }
+
+        public void Play()
+        {
+            SetPlayImageStateTo(false);
+            _playRoutine = StartCoroutine(PlayRoutineRoutine());
+        }
+
+        private IEnumerator PlayRoutineRoutine()
+        {
+            var startTime = Time.time - _slider.value * _timeDelta;
+
+            while (true)
+            {
+                var time = Mathf.Round(((Time.time - startTime) % 1f).Remap(0f, 1f, 0f, 36f));
+
+                _slider.value = time;
+
+                yield return null;
+            }
+        }
+
+        public void Pause()
+        {
+            StopCoroutine(_playRoutine);
+            _playRoutine = null;
+
+            SetPlayImageStateTo(true);
+        }
+
+        private void SetPlayImageStateTo(bool state)
+        {
+            _playImage.SetActive(state);
+            _pauseImage.SetActive(!state);
+        }
+
+        #region Event handlers
+        private void Slider_OnValueChanged(float value)
+        {
+            _timeText.text = $"{(_timeDelta * value).ToString(_nfi)} sec.";
+
+            Changed.Invoke((int)value);
+        }
+
+        private void PlayButton_OnClick()
+        {
+            if (_playRoutine == null)
+                Play();
+            else
+                Pause();
+        }
+        #endregion
+    }
+}
---
+++ Assets/Scripts/UI/Sequencing/TimelineLayoutGroup.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading.Tasks;
+using static UnityEngine.Debug;
+using UnityEngine.UI;
+
+namespace Namespace
+{
+	public class TimelineLayoutGroup : MonoBehaviour, ILayoutGroup
+	{
+        [SerializeField]
+        private RectTransform _rectTransform;
+
+        [SerializeField]
+        private RectTransform[] _points;
+        
+        private void Calculate()
+        {
+            var delta = _rectTransform.rect.width / (_points.Length - 1);
+
+            for (int i = 0; i < _points.Length; i++)
+            {
+                var pos = _points[i].anchoredPosition;
+                pos.x = delta * i;
+                _points[i].anchoredPosition = pos;
+            }
+        }
+
+        public void SetLayoutHorizontal()
+        {
+            Calculate();
+        }
+
+        public void SetLayoutVertical() { }
+
+        private void OnRectTransformDimensionsChange()
+        {
+            Calculate();
+        }
+
+#if UNITY_EDITOR
+        private void OnValidate()
+        {
+            Calculate();
+        }
+#endif
+    }
+}
